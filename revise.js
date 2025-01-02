4f7c6765d48f060c1a177e6fad34ce368f35f142
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-01-03 02:18:21 +0500

    reverse revis

--- revise.js
+++ revise.js
@@ -27,22 +27,34 @@
 
 // -------------------------------------------------findFactorial--------------------------------------
 
-const findFactorial = (num) => {
-    if (num < 0 || typeof num !== "number") {
-        return "invalid input"
-    } else if (num === 0) {
-        return 1
-    } else {
-        return num * findFactorial(num - 1)
-    }
-}
+// const findFactorial = (num) => {
+//     if (num < 0 || typeof num !== "number") {
+//         return "invalid input"
+//     } else if (num === 0) {
+//         return 1
+//     } else {
+//         return num * findFactorial(num - 1)
+//     }
+// }
 
-let res = findFactorial(5)
+// let res = findFactorial(5)
 
-console.log(res);
+// console.log(res);
 
 
 // 3. Reverse a String
+const reverseFun = (name) => {
+    let reverse = "";
+    for(let i = name.length-1; i >= 0; i--){
+        reverse += name[i]
+    }
+    return reverse
+}
+
+let name1 = "my name is jawad";
+let res = reverseFun(name1);
+console.log(res);
+
 
 // 4. Check Prime Number
 
