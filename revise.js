2488dcbf8e2834832e914228f7c5942f3c4be97e
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-01-04 01:46:46 +0500

    prime number logic

--- revise.js
+++ revise.js
@@ -41,22 +41,41 @@
 
 // console.log(res);
 
+// -------------------------------------------- reverseFun ------------------------------------------------
 
 // 3. Reverse a String
-const reverseFun = (name) => {
-    let reverse = "";
-    for(let i = name.length-1; i >= 0; i--){
-        reverse += name[i]
-    }
-    return reverse
-}
+// const reverseFun = (name) => {
+//     let reverse = "";
+//     for(let i = name.length-1; i >= 0; i--){
+//         reverse += name[i]
+//     }
+//     return reverse
+// }
 
-let name1 = "my name is jawad";
-let res = reverseFun(name1);
-console.log(res);
+// let name1 = "my name is jawad";
+// let res = reverseFun(name1);
+// console.log(res);
 
+// ----------------------------------------------------------------------------------------
 
 // 4. Check Prime Number
+
+const primeNumber = (num) => {
+    if (num <= 1) {
+        return "Negative or One number is not a prime number"
+    }
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return "it's not a prime number"
+        }
+    }
+    return "Yes it's a prime number"
+}
+
+
+let res = primeNumber(3);
+console.log(res);
 
 // 5. Find the Largest Number
 
